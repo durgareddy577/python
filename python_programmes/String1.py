d7cc4672593ffae807c77c9d31b5cd1a6b1d188f
durgareddy577 <durgareddy577.java@gmail.com>
2018-08-23 11:39:06 +0530

    python files are created

---
+++ python_programmes/String1.py
@@ -0,0 +1,9 @@
+first_name="durga";
+last_name='karri';
+# sentence="this is 'durga reddy' from banglore";
+# sentence="this is \"durga reddy\" from banglore";
+# sentence='this is \'durga reddy\' from banglore';
+sentence='this is "durga reddy" from banglore';
+print(first_name);
+print(last_name)
+print(sentence);
---
+++ python_programmes/boolean1.py
@@ -0,0 +1,13 @@
+a=0;
+b=20;
+name="";
+name1="durga"
+print(bool(a));
+
+print(bool(b));
+
+print(bool(name))
+
+print(bool(name1))
+
+print(bool(None))
