a755e2a9d108ab807e0174dcb4cdd083844246f0
durgareddy577 <durgareddy577.java@gmail.com>
2018-08-23 12:01:17 +0530

    string files

---
+++ python_programmes/String2.py
@@ -0,0 +1,9 @@
+name="durga reddy";
+print(name);
+
+name1="durga reddy"[2];
+print(name1);
+
+print(name[4]);
+
+print(name[-1]);
---
+++ python_programmes/string3.py
@@ -0,0 +1,52 @@
+name="durga reddy";
+
+name1="DURGA REDDY";
+
+sentence="1abc2abc3abc4abc5";
+
+sentence1="this is durga reddy from banglore";
+
+print(len(name)); #to find the length of the string
+
+print(name.upper()); #to covert lower case latter to uppercase latter
+
+print(name1.lower()) #TO CONVERT UPPER CASE LATTER TO LOWERCASE LATTERS
+
+print(sentence.replace("abc","xyz")); #to replace all occuaratons  strings with anothe string
+
+print(sentence.replace("abc","xyz",1));
+
+print(sentence.replace("abc","xyz",2));
+
+print(sentence1[8:13]);
+
+
+print(sentence1[8:13:1]);
+
+print(sentence1[8:13:3]);
+
+print(name[-1]);
+
+print(name[1]);
+
+print(name[:]);
+
+print(name[2:]);
+
+print(name[:4]);
+
+print(name[:-1]);
+
+print(name[::2]);
+
+print(name[::-1]);
+
+
+
+
+
+
+
+
+
+
